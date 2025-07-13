eb5413f7b0c3f479b47101360b827394a2eac854
Yossi <yyossi84@gmail.com>
2025-07-13 11:32:42 +0300

    final

--- app.js
+++ app.js
@@ -1,19 +1,63 @@
-const cart = document.getElementById("cart");
-let products = [];
+const cartBody = document.getElementById("cartBody");
 
-function add(event) {
+let products = JSON.parse(localStorage.getItem("products")) || [];
+updateTable();
+
+function addProduct(event) {
     event.preventDefault();
 
-    const name = document.getElementById("Name").value.trim();
-      const price =parseFloat(document.getElementById("Price").value.trim());
-      const category = document.getElementById("Category").value.trim();
-      const image = document.getElementById("Image").value.trim();
+    const data = collectDataFromForm();
+    if (!data) return;
 
+    products.push(data);
 
-     if(!name || isNaN(price) || !category || !image){
-        alert("Please fillall fields correctly.")
-        return;
-     }
+    localStorage.setItem("products", JSON.stringify(products));
 
-     
+    updateTable();
+    clearForm();
+}
+
+function collectDataFromForm() {
+    const name = document.getElementById("productName").value.trim();
+    const price = parseFloat(document.getElementById("productPrice").value);
+    const category = document.getElementById("productCategory").value;
+    const image = document.getElementById("productImage").value.trim();
+
+    if (!name || isNaN(price) || !category || !image) {
+        alert("Please fill in all fields correctly.");
+        return null;
+    }
+
+    return { name, price, category, image };
+}
+
+function updateTable() {
+    cartBody.innerHTML = "";
+
+    products.forEach((product, index) => {
+        cartBody.innerHTML += `
+      <tr>
+        <td>${product.name}</td>
+        <td>$${product.price.toFixed(2)}</td>
+        <td>${product.category}</td>
+        <td><img src="${product.image}" width="100"></td>
+        <td><button class="btn btn-danger btn-sm" onclick="deleteProduct(${index})">Delete</button></td>
+      </tr>
+    `;
+    });
+}
+
+function deleteProduct(index) {
+    products.splice(index, 1);
+
+    localStorage.setItem("products", JSON.stringify(products));
+
+    updateTable();
+}
+
+function clearForm() {
+    document.getElementById("productName").value = "";
+    document.getElementById("productPrice").value = "";
+    document.getElementById("productCategory").value = "";
+    document.getElementById("productImage").value = "";
 }
